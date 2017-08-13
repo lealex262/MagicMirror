573bf882ca8bf0d4c98463673740319958aa47f1
Jonathan Lian <jlian@gatech.edu>
2017-08-13 00:52:06 -0400

    ignore intellij, can save videos (no grayscale)

--- camera_test.py
+++ camera_test.py
@@ -1,20 +1,31 @@
 import numpy as np
 import cv2
 
+# find camera or webcam
 cap = cv2.VideoCapture(0)
 
-while(True):
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('test-save.avi', fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
+    if ret:
+        out.write(frame)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Any color conversions you want here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Display the frame; if color-converted,
+        # replace frame param with the converted frame, e.g. gray
+        cv2.imshow('Video Test', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 
 # When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
