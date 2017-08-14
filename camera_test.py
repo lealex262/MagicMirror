c5442eef746335db3b282076571f16be3962489a
Jonathan Lian <jlian@gatech.edu>
2017-08-14 02:12:14 -0400

    add unique image name generation

--- camera_test.py
+++ camera_test.py
@@ -1,32 +1,35 @@
 import numpy as np
 import cv2
 import picamera
-from time import sleep 
+import time
+import datetime
+from time import sleep
 
-def camera_functions():
+# find camera or webcam
+camera = picamera.PiCamera()
 
-    # find camera or webcam
-    camera = picamera.PiCamera()
+def save_video(videoName, timelength):
+    # Start Recording
+    camera.start_recording(videoName + ".h264")
+    sleep(timelength)
 
-    def save_video(videoName, timelength):
-        # Start Recording
-        camera.start_recording(videoName + ".h264")
-        sleep(timelength)
+    # Stop Recording
+    camera.stop_recording()
 
-        # Stop Recpording
-        camera.stop_recording()
+def take_image():
+    camera.resolution = (2592,1944)
+    camera.capture(generate_image_name() + ".jpg")
+    print("Picture taken")
 
-    def take_image(imageName):
-        camera.resolution = (2592,1944)
-        camera.capture(imageName + ".jpg")
-        print("Picture " + imageName + " taken")
-        
-    def take_grayscale_image(imageName):
-        camera.resolution = (2592,1944)
-        camera.color_effects = (128, 128)
-        camera.capture(imageName + ".jpg")
-        print("Picture " + imageName + " taken")
- 
-    save_video("test", 5)
-     
-camera_functions()
+def take_grayscale_image():
+    camera.resolution = (2592,1944)
+    camera.color_effects = (128, 128)
+    camera.capture(generate_image_name() + ".jpg")
+    print("Grayscale picture taken")
+
+def generate_image_name():
+    image_name = str(time.mktime(datetime.datetime.now().timetuple()))
+    return image_name
+
+# save_video("test", 5)
+take_image()
