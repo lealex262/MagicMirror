95c0d46bf574d0e2c065cd9d02b71b09a3b93f70
Jonathan Lian <jlian@gatech.edu>
2017-08-16 18:56:52 +0000

    Recognizing a fist -> take a picture -> upload -> delete twice -> back to video stream

--- python_code/camera_functions.py
+++ python_code/camera_functions.py
@@ -5,9 +5,6 @@ from time import mktime, sleep
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 
-# find camera or webcam
-camera = PiCamera()
-
 def save_video(videoName, timelength):
     # Start Recording
     camera.start_recording(videoName + ".h264")
@@ -16,16 +13,17 @@ def save_video(videoName, timelength):
     # Stop Recording
     camera.stop_recording()
 
-def take_image():
+def take_image(camera):
     camera.resolution = (2592,1944)
-    rawCapture = PiRGBArray(camera)
+    rawCapture = PiRGBArray(camera, size=(2592,1944))
     sleep(0.1)
-    camera.capture(rawCapture, format="bgr")
+    camera.capture(rawCapture, format="bgr", use_video_port=False)
     image = rawCapture.array
     image_path_extension = "../images/" + generate_image_name() + ".jpg"
     imwrite(image_path_extension, image)
     image_hosting.upload_picture(image_path_extension)
     print("Picture taken")
+    camera.close()
 
 def take_grayscale_image():
     camera.resolution = (2592,1944)
@@ -45,6 +43,6 @@ def generate_image_name():
 
 image_hosting.cron_scheduler()
 image_hosting.scheduler.start()
-while True:
-    if raw_input("Press a button to take an image\n"):
-        take_image()
+##while True:
+##    if raw_input("Press a button to take an image\n"):
+##        take_image()
--- python_code/detection.py
+++ python_code/detection.py
@@ -1,3 +1,4 @@
+import camera_functions
 import numpy as np
 import cv2
 from time import time, sleep
@@ -5,14 +6,14 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 
 def detect_fist():
-    timer_started = False
-    start_time = 0
-    end_time = 0
-    count = 0
     camera = PiCamera()
     camera.resolution = (1296,972)
     camera.framerate = 25
     rawCapture = PiRGBArray(camera, size=(1296,972))
+    timer_started = False
+    start_time = 0
+    end_time = 0
+    count = 0
     
     sleep(0.1)
     
@@ -20,11 +21,11 @@ def detect_fist():
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
         if timer_started:
             end_time = time()
-            # if it's been less than 5 seconds and we get 1 count of fist recognition, break
-            if end_time - start_time < 5 and count > 0:
-                return
-            # otherwise if it's been more than 5 seconds, reset the timer and count
-            elif end_time - start_time > 5:
+            # if it's been less than 7 seconds and we get 2 counts of fist recognition, break
+            if end_time - start_time < 7 and count > 1:
+                return initiate_countdown(camera)
+            # otherwise if it's been more than 7 seconds, reset the timer and count
+            elif end_time - start_time > 7:
                 timer_started = False
                 count = 0
         image = frame.array
@@ -49,9 +50,15 @@ def detect_fist():
         """
         
         rawCapture.truncate(0)
-    
-def take_image(imageName):
-    ret, frame = cap.read()
-    cv2.imwrite(imageName, frame)
+        
+def initiate_countdown(camera):
+    initial_count = 3
+    while initial_count > 0:
+        sleep(1)
+        print initial_count
+        initial_count -= 1    
+    print "Say Cheese!"
+    camera_functions.take_image(camera)
+    detect_fist()
 
 detect_fist()
